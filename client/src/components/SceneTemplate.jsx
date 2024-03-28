02317c1a8cdc575a2fe3ba07d40d7d296107159f
Ryan Suber <rsuber406@gmail.com>
2024-03-27 22:10:46 -0400

    should have a video now

--- client/src/components/SceneTemplate.jsx
+++ client/src/components/SceneTemplate.jsx
@@ -6,13 +6,13 @@ export default function SceneTemplate(props){
     const {title, img, shortDescription, id} = props
 
 
-
-
-
-   return(
-    <>
+    
+    
+    return(
+      <>
     <div className="sceneTemplate">
-      <img src={img}></img>
+    
+      <iframe width="100%" height={window.innerHeight / 5} src="https://www.youtube.com/embed/lkxLFMQNMVo?list=WL" title="Cinematic Boxing Commercial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       <h1>{title} </h1>
       <h2>{shortDescription} </h2>
     </div>
